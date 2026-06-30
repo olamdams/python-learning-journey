8be3a98d545482909ed60f0724a6c1d68fcc3d2b
olamdams <solamide2012@gmail.com>
2026-06-30 22:24:23 +0100

    Complete Lesson 3: elif statements

---
+++ 02_control_flow/day_of_week.py
@@ -0,0 +1,17 @@
+day_of_the_week = int(input("Enter a day of the week: "))
+if day_of_the_week == 1:
+    print("Monday")
+elif day_of_the_week == 2:
+    print("Tuesday")
+elif day_of_the_week == 3:
+    print("Wednesday")
+elif day_of_the_week == 4:
+    print("Thursday")
+elif day_of_the_week == 5:
+    print("Friday")
+elif day_of_the_week == 6:
+    print("Saturday")
+elif day_of_the_week == 7:
+    print("Sunday")
+else:
+    print("Invalid day number.")
