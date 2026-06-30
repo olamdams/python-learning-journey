319d9141e63e9947d3c1422d39bb6696e6e71910
olamdams <solamide2012@gmail.com>
2026-06-30 17:38:02 +0100

    Add if statement lesson

---
+++ 02_control_flow/if_statement.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are eligible to vote.")
+    if age >= 21:
+
+        print("You can legally drink alcohol in some countries.")
+
+number = int(input("Enter a your number: "))
+if number > 100:
+    print("It is a large number.")
+    
